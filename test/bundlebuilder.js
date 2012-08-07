cbd07dfc07332d2f997d3b2dcfa8e367a91dd899
ertoth <ertoth@paypal.com>
2012-08-07 16:49:37 -0700

    Refactoring, deal with multiple input types better, simplify api

--- test/bundlebuilder.js
+++ test/bundlebuilder.js
@@ -3,6 +3,7 @@
 'use strict';
 
 var should = require('should'),
+	fs = require('fs'),
 	BundleBuilder = require('../index');
 
 
@@ -10,19 +11,68 @@ describe('BundleBuilder', function () {
 
 
 	it('should convert JSON to properties correctly', function (next) {
-		BundleBuilder.convert( { file : './test/json/locales/us-EN/login.json', sourceType : 'json', targetType : 'properties' }, next );
+		BundleBuilder.convert('./test/json/locales/us-EN/login.json', 'json', 'properties', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
+	});
+
+	it('should convert JSON to properties file correctly', function (next) {
+
+		BundleBuilder.convert('./test/json/locales/us-EN/login.json', 'json', 'properties', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
 	});
 
 	it('should convert properties to json correctly', function (next) {
-		BundleBuilder.convert( { file : './test/properties/locales/us-EN/login.properties', sourceType : 'properties', targetType : 'json' }, next );
+		BundleBuilder.convert('./test/properties/locales/us-EN/login.properties', 'properties', 'json', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
 	});
 
 	it('should convert v4 to json correctly', function (next) {
-		BundleBuilder.convert( { file : './test/v4/locales/us-EN/login.4cb', sourceType : 'v4', targetType : 'json' }, next );
+		BundleBuilder.convert('./test/v4/locales/us-EN/login.4cb', 'v4', 'json', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
 	});
 
 	it('should convert v4 to properties correctly', function (next) {
-		BundleBuilder.convert( { file : './test/v4/locales/us-EN/login.4cb', sourceType : 'v4', targetType : 'properties' }, next );
+		BundleBuilder.convert('./test/v4/locales/us-EN/login.4cb', 'v4', 'properties', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
+	});
+
+	it('should deserialize JSON without error', function (next) {
+		BundleBuilder.deserialize('./test/json/locales/us-EN/login.json', 'json', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
+	});
+
+	it('should deserialize properties without error', function (next) {
+		BundleBuilder.deserialize('./test/properties/locales/us-EN/login.properties', 'properties', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
+	});
+
+	it('should deserialize 4cb without error', function (next) {
+		BundleBuilder.deserialize('./test/v4/locales/us-EN/login.4cb', 'v4', function (err, data) {
+			should.not.exist(err);
+			should.exist(data);
+			next();
+		});
 	});
 
 	// it('should convert json to v4 content correctly', function (next) {
