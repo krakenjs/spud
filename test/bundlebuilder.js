762b346590eea049d12b9330392bad2f2d5eab32
ertoth <ertoth@paypal.com>
2012-08-07 08:04:49 -0700

    Re-adding some tests.

--- test/bundlebuilder.js
+++ test/bundlebuilder.js
@@ -6,24 +6,24 @@ var should = require('should'),
 	BundleBuilder = require('../index');
 
 
-describe('JsonReader', function () {
+describe('BundleBuilder', function () {
 
 
 	it('should convert JSON to properties correctly', function (next) {
 		BundleBuilder.convert( { file : './test/json/locales/us-EN/login.json', sourceType : 'json', targetType : 'properties' }, next );
 	});
 
-	// it('should convert properties to json correctly', function (next) {
-	// 	BundleBuilder.convert( { file : './test/properties/locales/us-EN/login.properties', sourceType : 'properties', targetType : 'json' }, next );
-	// });
+	it('should convert properties to json correctly', function (next) {
+		BundleBuilder.convert( { file : './test/properties/locales/us-EN/login.properties', sourceType : 'properties', targetType : 'json' }, next );
+	});
 
 	it('should convert v4 to json correctly', function (next) {
 		BundleBuilder.convert( { file : './test/v4/locales/us-EN/login.4cb', sourceType : 'v4', targetType : 'json' }, next );
 	});
 
-	// it('should convert v4 to properties correctly', function (next) {
-	// 	BundleBuilder.convert( { file : './test/v4/locales/us-EN/login.4cb', sourceType : 'v4', targetType : 'properties' }, next );
-	// });
+	it('should convert v4 to properties correctly', function (next) {
+		BundleBuilder.convert( { file : './test/v4/locales/us-EN/login.4cb', sourceType : 'v4', targetType : 'properties' }, next );
+	});
 
 	// it('should convert json to v4 content correctly', function (next) {
 	// 	BundleBuilder.convert( { file : './test/json/locales/us-EN/login.json', sourceType : 'json', targetType : 'v4' }, next );
